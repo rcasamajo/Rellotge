b881cd86e8542f434d7ef489a6e7424a4d1ff055
rcasamajo <rcasamajo@iespoblenou.org>
2017-01-20 18:45:09 +0100

    Timer

--- Rellotge/MainWindow.xaml.cs
+++ Rellotge/MainWindow.xaml.cs
@@ -1,17 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace Rellotge
 {
@@ -23,6 +12,23 @@ namespace Rellotge
         public MainWindow()
         {
             InitializeComponent();
+
+            lblTime.Content = DateTime.Now.ToLongTimeString();
+
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += timer_Tick;
+            timer.Start();
+        }
+
+        void timer_Tick(object sender, EventArgs e)
+        {
+            lblTime.Content = DateTime.Now.ToLongTimeString();
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
         }
     }
 }
