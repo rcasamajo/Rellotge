548b9d89e1aaef4f8b353d0f8a8e833a3a7474d5
rcasamajo <rcasamajo@iespoblenou.org>
2017-01-23 20:45:39 +0100

    Persistència alarma

---
+++ Rellotge/Alarma.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rellotge
+{
+    [Serializable()]
+    class Alarma
+    {
+        private String alarma;
+        private bool activa;
+
+        public Alarma()
+        {
+            this.alarma = "00:00";
+            activa = false;
+        }
+
+        public Alarma(String alarma, bool activa)
+        {
+            this.alarma = alarma;
+            this.activa = activa;
+        }
+
+        public String horaAlarma
+        {
+            get { return alarma; }
+            set { alarma = value; }
+        }
+
+        public bool alarmaActiva { get; set; }
+    }
+}
--- Rellotge/MainWindow.xaml.cs
+++ Rellotge/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Windows;
 using System.Windows.Threading;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 
 namespace Rellotge
 {
@@ -9,21 +11,48 @@ namespace Rellotge
     /// </summary>
     public partial class MainWindow : Window
     {
+        const string FileName = @"..\..\SavedAlarm.bin";
+        private Alarma alarm;
+
         public MainWindow()
         {
+            alarm = new Alarma();
+            alarm.horaAlarma = "11:11";
+            alarm.alarmaActiva = false;
+
             InitializeComponent();
 
             lblTime.Content = DateTime.Now.ToLongTimeString();
 
             DispatcherTimer timer = new DispatcherTimer();
             timer.Interval = TimeSpan.FromSeconds(1);
-            timer.Tick += timer_Tick;
+            timer.Tick += Timer_Tick;
             timer.Start();
         }
 
-        void timer_Tick(object sender, EventArgs e)
+        void Timer_Tick(object sender, EventArgs e)
         {
             lblTime.Content = DateTime.Now.ToLongTimeString();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (File.Exists(FileName))
+            {
+                Stream TestFileStream = File.OpenRead(FileName);
+                BinaryFormatter deserializer = new BinaryFormatter();
+                alarm = (Alarma)deserializer.Deserialize(TestFileStream);
+                TestFileStream.Close();
+            }
+
+            TBAlarma.Text = alarm.horaAlarma;
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            Stream TestFileStream = File.Create(FileName);
+            BinaryFormatter serializer = new BinaryFormatter();
+            serializer.Serialize(TestFileStream, alarm);
         }
 
         private void MenuItem_Click(object sender, RoutedEventArgs e)
@@ -34,6 +63,11 @@ namespace Rellotge
         private void MenuItem_Click_1(object sender, RoutedEventArgs e)
         {
             MessageBox.Show("Rellotge digital de DAM2","Rellotge");
+        }
+
+        private void tbAlarma_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            alarm.horaAlarma = TBAlarma.Text;
         }
     }
 }
